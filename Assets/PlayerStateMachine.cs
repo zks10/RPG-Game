e68add9e9ca7b6293f4f7f9a419b3a03548c9a6f
zks10 <kevinzhu20041010@gmail.com>
2025-10-14 20:21:13 -0400

    Initial clean Unity commit

---
+++ Assets/PlayerStateMachine.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerStateMachine 
+{
+    public PlayerState currentState
+    {
+        get; private set;
+    }
+    public void Initialize(PlayerState _startState)
+    {
+        currentState = _startState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(PlayerStateMachine _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
+    }
+
+}
