97f797f6d42eff7edb0202b8302e26b9eee9a24f
zks10 <kevinzhu20041010@gmail.com>
2025-11-13 18:57:58 -0500

    added the Options UI design (wait to add more features)

---
+++ Assets/Scripts/UI Design/Canvas Menu/UI_Options.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UI_Options : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
